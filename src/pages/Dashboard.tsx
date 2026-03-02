614f3a6328df3af18b91c2d5ae58dcc9431bf2cd
Alex Xiang <xiangzhe0812@gmail.com>
2026-03-03 08:50:14 +1000

    add cosmos wallet address to wallet panel

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -956,6 +956,7 @@ const Dashboard: React.FC = () => {
                                     onCreateWallet={handleCreateNewWallet}
                                     onImportWallet={() => setShowCosmosImportModal(true)}
                                     usdcBalance={getTransferTokenBalance()}
+                                    cosmosWalletAddress={cosmosWallet.address}
                                 />
                                 <TransactionPanel cosmosWalletAddress={cosmosWallet.address} usdcBalance={getTransferTokenBalance()} />
                             </div>
